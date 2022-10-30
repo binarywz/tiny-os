13b229049f009357cd6c50597503d3a7601279ff
binarywz <binarywz@163.com>
2022-10-30 18:27:25 +0800

    switch dpl

--- os/source/os.c
+++ os/source/os.c
@@ -20,12 +20,22 @@ uint32_t pg_dir[1024] __attribute__((aligned(4096))) = {
     [0] = (0) | PDE_P | PDE_PS | PDE_W | PDE_U,	    // PDE_PS，开启4MB的页，恒等映射
 };
 
+/**
+ * @brief 任务0和1的栈空间
+ */
+uint32_t task_dpl3_stack[1024];
+
 struct {uint16_t offset_l, selector, attr, offset_h;} idt_table[256] __attribute__((aligned(8))) = {1};
 struct {uint16_t limit_l, base_l, basehl_attr, base_limit;} gdt_table[256] __attribute__((aligned(8))) = {
     // 0x00cf9a000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，32位代码段（非一致代码段），界限4G，
     [KERNEL_CODE_SEG / 8] = {0xffff, 0x0000, 0x9a00, 0x00cf},
     // 0x00cf93000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，数据段，界限4G，可读写
     [KERNEL_DATA_SEG / 8] = {0xffff, 0x0000, 0x9200, 0x00cf},
+
+    // 0x00cffa000000ffff - 从0地址开始，P存在，DPL=3，Type=非系统段，32位代码段，界限4G
+    [APP_CODE_SEG/ 8] = {0xffff, 0x0000, 0xfa00, 0x00cf},
+    // 0x00cff3000000ffff - 从0地址开始，P存在，DPL=3，Type=非系统段，数据段，界限4G，可读写
+    [APP_DATA_SEG/ 8] = {0xffff, 0x0000, 0xf300, 0x00cf},
 };
 
 /**
